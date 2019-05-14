e6f6195ef4e39608f52808d19f16826da1e9e6a1
huseyincotel <huseyincotel09@gmail.com>
2019-05-14 09:37:07 +0300

    numpy lstm

--- lstm_autoencoder.py
+++ lstm_autoencoder.py
@@ -1,10 +1,20 @@
 import tensorflow as tf
+import numpy as np
 from tensorflow.contrib.rnn import BasicLSTMCell
 
 #tf.nn.embedding_lookup
 #tf.gradients
 #tf.trainable_variables
 #optimizer apply gradient
+
+def sigmoid(x):
+    return (1 / (1 + np.exp(-x)))
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0)
+
 class LSTMAutoEncoder():
     def __init__(self, batch_size, padding_size, embedding_size, vocab_size):
         self.batch_size = batch_size
@@ -24,11 +34,46 @@ class LSTMAutoEncoder():
 
     def create_graph(self):
         embed_output = tf.nn.embedding_lookup(self.embeddings, self.input, name='Embedding Layer')
+        lstm = BasicLSTMCell(1)
+        state = lstm.zero_state(self.batch_size, dtype=tf.float32)
 
     def run_graph(self, batch):
         self.feed_dict[self.input] = batch
         self.sess.run(self.output, feed_dict=self.feed_dict)
+
         return self.output
+
+    # def LSTM_Cell(self, input, h_prev, c_prev, parameters):
+    #     # input (128,1)  h_prev (256,1)
+    #     input_size, batch_size = input.shape
+    #     h_size, _ = h_prev.shape
+    #
+    #     #Retrieve parameters
+    #     Wf = parameters["Wf"]
+    #     bf = parameters["bf"]
+    #     Wi = parameters["Wi"]
+    #     bi = parameters["bi"]
+    #     Wc = parameters["Wc"]
+    #     bc = parameters["bc"]
+    #     Wo = parameters["Wo"]
+    #     bo = parameters["bo"]
+    #     Wy = parameters["Wy"]
+    #     by = parameters["by"]
+    #
+    #     concat = np.zeros((input_size + h_size, batch_size))
+    #     concat[: input_size, :] = input
+    #     concat[input_size:, :] = h_prev
+    #
+    #     ft = sigmoid(np.dot(Wf, concat) + bf)
+    #     it = sigmoid(np.dot(Wi, concat) + bi)
+    #     cct = np.tanh(np.dot(Wc, concat) + bc)
+    #     c_next = ft * c_prev + it * cct
+    #     ot = sigmoid(np.dot(Wo, concat) + bo)
+    #     h_next = ot * np.tanh(c_next)
+    #
+    #     yt_pred = softmax(np.dot(Wy, h_next) + by)
+
+
 
 
 
